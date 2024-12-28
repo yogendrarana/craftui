0f466205ce44aca973d2b48238b14382187b0252
Yogendra Rana <yogendrarana.tech@gmail.com>
2024-12-28 23:29:51 +0545

    added separator component

--- src/config/docs.ts
+++ src/config/docs.ts
@@ -36,27 +36,27 @@ export const docsConfig: DocsConfig = {
             items: [
                 {
                     title: "Buttons",
-                    href: `/docs/elements/button`,
+                    href: "/docs/elements/button",
                     items: []
                 },
                 {
                     title: "Loaders",
-                    href: `/docs/elements/loader`,
+                    href: "/docs/elements/loader",
                     items: []
                 },
                 {
                     title: "Checkbox",
-                    href: `/docs/elements/checkbox`,
+                    href: "/docs/elements/checkbox",
                     items: []
                 },
                 {
                     title: "Toggle Switch",
-                    href: `/docs/elements/toggle-switch`,
+                    href: "/docs/elements/toggle-switch",
                     items: []
                 },
                 {
                     title: "Input & Textarea",
-                    href: `/docs/elements/input-textarea`,
+                    href: "/docs/elements/input-textarea",
                     items: []
                 }
             ]
@@ -66,47 +66,51 @@ export const docsConfig: DocsConfig = {
             items: [
                 {
                     title: "Accordion",
-                    href: `/docs/components/accordion`
+                    href: "/docs/components/accordion"
                 },
                 {
                     title: "Button",
-                    href: `/docs/components/button`
+                    href: "/docs/components/button"
                 },
                 {
                     title: "Confetti",
-                    href: `/docs/components/confetti`
+                    href: "/docs/components/confetti"
                 },
                 {
                     title: "Cursor",
-                    href: `/docs/components/custom-cursor`
+                    href: "/docs/components/custom-cursor"
                 },
                 {
                     title: "Dialog",
-                    href: `/docs/components/dialog`
+                    href: "/docs/components/dialog"
                 },
                 {
                     title: "Dock",
-                    href: `/docs/components/dock`
+                    href: "/docs/components/dock"
                 },
                 {
                     title: "Drawer",
-                    href: `/docs/components/drawer`
+                    href: "/docs/components/drawer"
                 },
                 {
                     title: "Marquee",
-                    href: `/docs/components/marquee`
+                    href: "/docs/components/marquee"
                 },
                 {
                     title: "Magnetic",
-                    href: `/docs/components/magnetic`
+                    href: "/docs/components/magnetic"
+                },
+                {
+                    title: "Separator",
+                    href: "/docs/components/separator"
                 },
                 {
                     title: "Tabs",
-                    href: `/docs/components/tabs`
+                    href: "/docs/components/tabs"
                 },
                 {
                     title: "Tooltip",
-                    href: `/docs/components/tooltip`
+                    href: "/docs/components/tooltip"
                 }
             ]
         },
@@ -115,12 +119,12 @@ export const docsConfig: DocsConfig = {
             items: [
                 {
                     title: "Typing Effect",
-                    href: `/docs/text/typing-effect`,
+                    href: "/docs/text/typing-effect",
                     items: []
                 },
                 {
                     title: "Text Scramble",
-                    href: `/docs/text/text-scramble`,
+                    href: "/docs/text/text-scramble",
                     items: []
                 }
             ]
---
+++ src/content/examples/separator-demo.tsx
@@ -0,0 +1,20 @@
+import { Separator } from "../registry/core/separator";
+
+export default function SeparatorDemo() {
+    return (
+        <div className="space-y-6">
+            <div>
+                <h4 className="text-sm font-medium leading-none">Horizontal</h4>
+                <p className="text-sm text-muted-foreground">A horizontal separator</p>
+                <Separator className="my-4" />
+            </div>
+            <div className="flex h-5 items-center space-x-4 text-sm">
+                <div>Item 1</div>
+                <Separator orientation="vertical" />
+                <div>Item 2</div>
+                <Separator orientation="vertical" />
+                <div>Item 3</div>
+            </div>
+        </div>
+    );
+}
