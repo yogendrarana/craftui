6edd3d90b88f7644add14cf5aba7bda9358b1804
Yogendra Rana <yogendrarana.tech@gmail.com>
2024-12-28 23:01:26 +0545

    chore

--- src/content/registry/core/button.tsx
+++ src/content/registry/core/button.tsx
@@ -5,15 +5,16 @@ import { cn } from "@/lib/utils";
 import { cva, type VariantProps } from "class-variance-authority";
 
 const buttonVariants = cva(
-    "inline-flex items-center justify-center rounded-sm text-sm font-medium ring-offset-background transition-colors focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-ring focus-visible:ring-offset-2 disabled:pointer-events-none disabled:opacity-50",
+    "inline-flex items-center justify-center gap-2 whitespace-nowrap rounded text-sm font-medium ring-offset-background transition-colors focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-ring focus-visible:ring-offset-2 disabled:pointer-events-none disabled:opacity-50 [&_svg]:pointer-events-none [&_svg]:size-4 [&_svg]:shrink-0",
     {
         variants: {
             variant: {
-                default: "bg-primary text-white hover:bg-primary-dark",
-                danger: "bg-red-500 text-white hover:bg-red-600",
-                outline: "border border-gray-300 bg-white text-gray-700 hover:bg-gray-100",
-                secondary: "bg-gray-200 text-gray-900 hover:bg-gray-300",
-                link: "text-primary underline"
+                default: "bg-primary text-primary-foreground hover:bg-primary/90",
+                danger: "bg-destructive text-destructive-foreground hover:bg-destructive/90",
+                secondary: "bg-secondary text-secondary-foreground hover:bg-secondary/80",
+                outline:
+                    "border border-input bg-background hover:bg-accent hover:text-accent-foreground",
+                link: "text-primary underline-offset-4 underline"
             },
             size: {
                 default: "h-9 px-4 py-2",
