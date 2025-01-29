4fbbc26aac4f7e7ca2bc68cfb53be81d70f60d53
Yogendra Rana <yogendrarana.tech@gmail.com>
2025-01-29 22:06:57 +0545

    Added first version of timeline component

---
+++ src/content/examples/timeline-fill-marker.tsx
@@ -0,0 +1,44 @@
+import {
+    Timeline,
+    TimelineItem,
+    TimelineMarker,
+    TimelineContent,
+    TimelineTitle,
+    TimelineDescription
+} from "../registry/components/timeline";
+
+export default function App() {
+    return (
+        <Timeline variant="vertical" className="max-w-md mx-auto">
+            <TimelineItem>
+                <TimelineMarker variant="fill" />
+                <TimelineContent>
+                    <TimelineTitle>Step 1</TimelineTitle>
+                    <TimelineDescription>
+                        This is the first step of the process.
+                    </TimelineDescription>
+                </TimelineContent>
+            </TimelineItem>
+
+            <TimelineItem>
+                <TimelineMarker variant="fill" />
+
+                <TimelineContent>
+                    <TimelineTitle>Step 2</TimelineTitle>
+                    <TimelineDescription>Intermediate stage of the timeline.</TimelineDescription>
+                </TimelineContent>
+            </TimelineItem>
+
+            <TimelineItem>
+                <TimelineMarker variant="fill" />
+
+                <TimelineContent>
+                    <TimelineTitle>Final Step</TimelineTitle>
+                    <TimelineDescription>
+                        Congratulations! You’ve completed the journey.
+                    </TimelineDescription>
+                </TimelineContent>
+            </TimelineItem>
+        </Timeline>
+    );
+}
---
+++ src/content/examples/timeline-horizontal.tsx
@@ -0,0 +1,43 @@
+import {
+    Timeline,
+    TimelineItem,
+    TimelineMarker,
+    TimelineContent,
+    TimelineTitle,
+    TimelineDescription
+} from "../registry/components/timeline";
+
+export default function TimelineDemo() {
+    return (
+        <Timeline variant="horizontal">
+            <TimelineItem>
+                <TimelineMarker variant="fill" />
+                <TimelineContent>
+                    <TimelineTitle>2022</TimelineTitle>
+                    <TimelineDescription>Company Founded</TimelineDescription>
+                </TimelineContent>
+            </TimelineItem>
+            <TimelineItem>
+                <TimelineMarker variant="fill" />
+                <TimelineContent>
+                    <TimelineTitle>2023</TimelineTitle>
+                    <TimelineDescription>First Major Client</TimelineDescription>
+                </TimelineContent>
+            </TimelineItem>
+            <TimelineItem>
+                <TimelineMarker variant="fill" />
+                <TimelineContent>
+                    <TimelineTitle>2024</TimelineTitle>
+                    <TimelineDescription>Expanded to 50 Employees</TimelineDescription>
+                </TimelineContent>
+            </TimelineItem>
+            <TimelineItem>
+                <TimelineMarker variant="empty" />
+                <TimelineContent>
+                    <TimelineTitle>2025</TimelineTitle>
+                    <TimelineDescription>International Expansion</TimelineDescription>
+                </TimelineContent>
+            </TimelineItem>
+        </Timeline>
+    );
+}
